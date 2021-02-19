175064a23916fe6a7958fa330a9e1ed915925411
Gareth <garethfultz@gmail.com>
2021-02-19 15:29:07 -0500

    Fixed Errors with Main Fucntion
    Fixed an error where the main function would not properly read images
    
    Fixed an error where the main function would not properly handle images without alpha values.

--- Main/main.py
+++ Main/main.py
@@ -1,16 +1,23 @@
 import sys
 from cv2 import cv2 
 from os import listdir, path
+import numpy as np
 import functions
 
-TEST_MODE = False
+TEST_MODE = True
 
 #Main function for data Collection
 def main():
-    dir_path = path.dirname(path.realpath(__file__)) + "\\..\\Logos"
+    dir_path = path.dirname(path.realpath(__file__)) + "\\..\\Logos\\"
 
     for imgPath in listdir(dir_path):
-        logo = Logo(cv2.imread(dir_path + imgPath, cv2.IMREAD_UNCHANGED), imgPath)
+        
+        img = cv2.imread(dir_path + imgPath, cv2.IMREAD_UNCHANGED)
+
+        if (len(img[0,0]) < 4):
+            img = AddAlphaChannel(img)
+ 
+        logo = Logo(img, imgPath)
         
         for function in functions.ExportFunctions:
             function(logo, False)
@@ -19,10 +26,15 @@ def main():
 #Test function to test individual features
 def test():
     #Passes each logo into each function in the TestFunction array
-    dir_path = path.dirname(path.realpath(__file__)) + "\\..\\TestLogos"
+    dir_path = path.dirname(path.realpath(__file__)) + "\\..\\TestLogos\\"
 
     for imgPath in listdir(dir_path):
-        logo = Logo(cv2.imread(dir_path + imgPath, cv2.IMREAD_UNCHANGED), imgPath)
+        img = cv2.imread(dir_path + imgPath, cv2.IMREAD_UNCHANGED)
+
+        if (len(img[0,0]) < 4):
+            img = AddAlphaChannel(img)
+ 
+        logo = Logo(img, imgPath)
 
         for function in functions.TestFunctions:
             function(logo, False)
@@ -36,7 +48,12 @@ class Logo:
         self.name = name
         self.attributes = {}
 
-
+def AddAlphaChannel(img):
+    b_channel, g_channel, r_channel = cv2.split(img)
+    alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255
+    img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+    return img_BGRA
+    
 
 if __name__ == "__main__":
     
